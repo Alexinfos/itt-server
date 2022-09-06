9529787a001cc629e0314318f7f41b1e9542c307
alexisbn <alexinfos@free.fr>
2022-09-06 11:15:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+from re import split
 import tweepy
 
 import credentials
 
-class IDPrinter(tweepy.StreamingClient):
+class TweetHandler(tweepy.StreamingClient):
 
     def on_tweet(self, tweet):
-        print(tweet.text)
         print("---")
+        print(tweet.text)
+        
 
-printer = IDPrinter(credentials.bearerToken)
+        test = tweet.text.split("\n")
+
+        print("---Analyse:")
+        for e in test:
+            print(e)
+            print("-")
+
+handler = TweetHandler(credentials.bearerToken)
 
 
 # @TAG_InfoTrafic: 2338153482
-printer.add_rules(tweepy.StreamRule("from:2338153482"))
-printer.filter()
+print("Listening for new tweets from @TAG_InfoTrafic")
+handler.add_rules(tweepy.StreamRule("from:2338153482"))
+handler.filter()
